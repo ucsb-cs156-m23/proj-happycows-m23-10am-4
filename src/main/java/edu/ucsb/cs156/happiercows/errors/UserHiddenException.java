7b56f062d988f5a9b2b5ffeca784a71672c8281a
publicqi <qisu@ucsb.edu>
2023-08-20 04:11:12 -0700

    feat: add exception for hidden user

---
+++ src/main/java/edu/ucsb/cs156/happiercows/errors/UserHiddenException.java
@@ -0,0 +1,7 @@
+package edu.ucsb.cs156.happiercows.errors;
+
+public class UserHiddenException extends Exception {
+    public UserHiddenException(String messageString){
+        super(messageString);
+    }
+}
