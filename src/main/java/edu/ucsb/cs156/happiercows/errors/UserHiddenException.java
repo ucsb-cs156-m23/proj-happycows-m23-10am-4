1394c82c0ad80a1d5dcdbf2d8b48242921356dca
publicqi <qisu@ucsb.edu>
2023-08-20 21:14:18 -0700

    fix: remove unused exception constructor

--- src/main/java/edu/ucsb/cs156/happiercows/errors/UserHiddenException.java
+++ src/main/java/edu/ucsb/cs156/happiercows/errors/UserHiddenException.java
@@ -1,10 +1,6 @@
 package edu.ucsb.cs156.happiercows.errors;
 
 public class UserHiddenException extends RuntimeException {
-    public UserHiddenException(String messageString){
-        super(messageString);
-    }
-
     public UserHiddenException(long userId) {
         super("User with id " + userId + " is hidden");
     }
