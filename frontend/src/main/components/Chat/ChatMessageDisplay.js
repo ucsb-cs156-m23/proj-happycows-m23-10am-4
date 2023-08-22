080d7889b8457dcab6ef28dd67493ef5e0ba8404
iain <cakefoxmc@gmail.com>
2023-08-21 21:33:17 -0700

    iw - removed prop mutation

--- frontend/src/main/components/Chat/ChatMessageDisplay.js
+++ frontend/src/main/components/Chat/ChatMessageDisplay.js
@@ -1,22 +1,19 @@
 import { Card } from 'react-bootstrap';
 
 const ChatMessageDisplay = ({ message, testId }) => {
-  if (!message.username) {
-    message.username = "Anonymous";
-  }
-  if (message.timestamp) {
-    message.timestamp = message.timestamp.replace('T', ' ').split('.')[0];
-  }
+  const username = message.username || "Anonymous";
+
+  const formattedTimestamp = message.timestamp ? message.timestamp.replace('T', ' ').split('.')[0] : '';
 
   return (
     <Card data-testid={testId}>
       <Card.Body>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <Card.Title data-testid={`${testId}-User`} style={{ margin: 0 }}>
-            {message.username} ({message.userId})
+            {username} ({message.userId})
           </Card.Title>
           <Card.Subtitle data-testid={`${testId}-Date`} style={{ margin: 0 }}>
-            {message.timestamp}
+            {formattedTimestamp}
           </Card.Subtitle>
         </div>
         <Card.Text data-testid={`${testId}-Message`}>{message.message}</Card.Text>
