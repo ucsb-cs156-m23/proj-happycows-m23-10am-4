0ba49771c99c378acda8c0908b9da98cf92dca1e
Yuanchen Meng <mych12345@126.com>
2023-08-22 03:50:25 -0700

    ym - feat: simple paged component
    Not finished, a lot Todos

---
+++ frontend/src/main/components/Commons/PagedProfitsTable.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import React from 'react';
+import { Pagination } from 'react-bootstrap';
+
+
+const PagedProfitsTable = ({ data, onPageChange}) => {
+    //eslint - disable next line
+    const { content, pageable, _ , totalPages, totalElements} = data;
+    console.log(content);
+
+    return (
+        <div>
+            <ul>
+                {content && content.map((item) => (
+                    <li key={item.id}>
+                        <p>Amount: {item.amount}</p>
+                        <p>Timestamp: {item.timestamp}</p>
+                        <p>Number of Cows: {item.numCows}</p>
+                        <p>Average Cow Health: {item.avgCowHealth}</p>
+                    </li>
+                ))}
+            </ul>
+
+            {/* Pagination component */}
+            <Pagination>
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <Pagination.Item
+                        key={index}
+                        active={index === pageable['pageNumber']}
+                        onClick={() => onPageChange(index)}
+                    >
+                        {index + 1}
+                    </Pagination.Item>
+                ))}
+            </Pagination>
+        </div>
+    );
+};
+
+export default PagedProfitsTable;
--- frontend/src/main/pages/DemoPlayPage.js
+++ frontend/src/main/pages/DemoPlayPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable */
+import React, { useState, useEffect } from "react";
 import { Container, CardGroup } from "react-bootstrap";
 import { toast } from "react-toastify";
 
@@ -8,9 +9,14 @@ import CommonsPlay from "main/components/Commons/CommonsPlay";
 import FarmStats from "main/components/Commons/FarmStats";
 import ManageCows from "main/components/Commons/ManageCows";
 import Profits from "main/components/Commons/Profits";
+
+
+import PagedProfitsTable from "main/components/Commons/PagedProfitsTable";
 import dummy from "assets/UserHomeDummy.json";
 
 export default function DemoPlayPage() {
+
+    const [currentPage, setCurrentPage] = useState(0);
 
     // eslint-disable-next-line no-unused-vars
     const  commonsId  = 0;
@@ -21,6 +27,8 @@ export default function DemoPlayPage() {
     const userCommonsProfits = dummy['Transactions'];
     //const userPagedProfits = dummy['PagedTransactions'];
 
+
+
     const onBuy = () => {
         toast(`Buy cow demo`);
     };
@@ -29,10 +37,22 @@ export default function DemoPlayPage() {
         toast(`Sell cow demo`);
     };
 
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+        // TODO Fetch data for the new page and update the data object
+    };
+
 
     return (
         <div data-testid="playpage-div">
             <BasicLayout >
+                <PagedProfitsTable data={dummy['PagedTransactions']}  onPageChange={handlePageChange}></PagedProfitsTable>
+            </BasicLayout>
+        </div>
+    )
+}
+
+/*
                 <Container >
                     <CommonsPlay currentUser={currentUser} />
                     <CommonsOverview commonsPlus={commonsPlus} currentUser={currentUser} />
@@ -43,7 +63,4 @@ export default function DemoPlayPage() {
                         <Profits userCommons={userCommons} profits={userCommonsProfits} />
                     </CardGroup>
                 </Container>
-            </BasicLayout>
-        </div>
-    )
-}
+* */
