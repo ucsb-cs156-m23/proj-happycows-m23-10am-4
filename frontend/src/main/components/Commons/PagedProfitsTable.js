3fa347ceaa4c81f0ad2e524580ff3b312d3eba36
Yuanchen Meng <mych12345@126.com>
2023-08-22 12:43:04 -0700

    ym - ending trials

--- frontend/src/main/components/Commons/PagedProfitsTable.js
+++ frontend/src/main/components/Commons/PagedProfitsTable.js
@@ -17,37 +17,6 @@ const PagedProfitsTable = ({ data, onPageChange}) => {
     console.log(pageable);
 
 
-    const renderPageItems = () => {
-        const pageItems = [];
-
-        // Display Ellipsis if needed before the first item
-        if (pageable.pageNumber >= 3) {
-            pageItems.push(<Pagination.Ellipsis key="ellipsis-start" />);
-        }
-
-        // Add the two previous pages and the current active page
-        for (let i = pageable.pageNumber - 2; i <= pageable.pageNumber; i++) {
-            if (i >= 0) {
-                pageItems.push(
-                    <Pagination.Item
-                        key={i}
-                        active={i === pageable.pageNumber}
-                        onClick={() => onPageChange(i)}
-                    >
-                        {i + 1}
-                    </Pagination.Item>
-                );
-            }
-        }
-
-        // Add Ellipsis if needed after the current active page
-        if (pageable.pageNumber + 3 < totalPages) {
-            pageItems.push(<Pagination.Ellipsis key="ellipsis-end" />);
-        }
-
-        return pageItems;
-    };
-
     const profitsForTable =
         content ?
             content.map(item => ({
@@ -105,16 +74,6 @@ const PagedProfitsTable = ({ data, onPageChange}) => {
                     />
                 </Card.Body>
             </Card>
-            <ul>
-                {content && content.map((item) => (
-                    <li key={item.id}>
-                        <p>Amount: {item.amount}</p>
-                        <p>Timestamp: {item.timestamp}</p>
-                        <p>Number of Cows: {item.numCows}</p>
-                        <p>Average Cow Health: {item.avgCowHealth}</p>
-                    </li>
-                ))}
-            </ul>
 
             {/* Pagination component */}
             <Pagination>
@@ -161,4 +120,16 @@ export default PagedProfitsTable;
                     </Pagination.Item>
                 ))}
             </Pagination>
+
+
+             <ul>
+                {content && content.map((item) => (
+                    <li key={item.id}>
+                        <p>Amount: {item.amount}</p>
+                        <p>Timestamp: {item.timestamp}</p>
+                        <p>Number of Cows: {item.numCows}</p>
+                        <p>Average Cow Health: {item.avgCowHealth}</p>
+                    </li>
+                ))}
+            </ul>
 */
