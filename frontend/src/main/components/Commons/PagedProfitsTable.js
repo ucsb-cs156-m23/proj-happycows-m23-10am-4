7431ae7883ede94fdcbc9851ca57da0e00de55ff
Yuanchen Meng <mych12345@126.com>
2023-08-22 11:46:14 -0700

    ym - trial 12

--- frontend/src/main/components/Commons/PagedProfitsTable.js
+++ frontend/src/main/components/Commons/PagedProfitsTable.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
+import ProfitsTable from "./ProfitsTable";
+import {timestampToDate} from "../../utils/dateUtils";
+import OurTable from "../OurTable";
+import {parseMoney} from "main/utils/MoneyParsing"
 
 
 const PagedProfitsTable = ({ data, onPageChange}) => {
@@ -43,10 +48,63 @@ const PagedProfitsTable = ({ data, onPageChange}) => {
         return pageItems;
     };
 
+    const profitsForTable =
+        content ?
+            content.map(item => ({
+                date: timestampToDate(item.timestamp),
+                ...item
+            })) :
+            // Stryker disable next-line ArrayDeclaration : no need to test what happens if [] is replaced with ["Stryker was here"]
+            [];
+    profitsForTable.reverse();
+
+
+
+    // Stryker disable ArrayDeclaration : [columns] and [students] are performance optimization; mutation preserves correctness
+    const memoizedColumns = React.useMemo(() =>
+            [
+                {
+                    Header: "Profit",
+                    accessor: (row) => `$${parseMoney(row.amount)}`,
+                },
+                {
+                    Header: "Date",
+                    accessor: "date",
+                },
+                {
+                    Header: "Health",
+                    accessor: (row) => `${row.avgCowHealth.toFixed(2) + '%'}`
+                },
+                {
+                    Header: "Cows",
+                    accessor: "numCows",
+                },
+            ],
+        []);
+    const memoizedDates = React.useMemo(() => profitsForTable, [profitsForTable]);
+    // Stryker restore ArrayDeclaration
+
+
+
 
 
     return (
         <div>
+            <Card>
+                <Card.Header as="h5">
+                    Profits
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>
+                        You will earn profits from milking your cows everyday at 4am.
+                    </Card.Title>
+                    <OurTable
+                        data={memoizedDates}
+                        columns={memoizedColumns}
+                        testid={"ProfitsTable"}
+                    />
+                </Card.Body>
+            </Card>
             <ul>
                 {content && content.map((item) => (
                     <li key={item.id}>
